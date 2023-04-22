135014a3c76918ca9c0900aef70ebb27fc6c505c
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2023-04-22 18:39:01 -0300

    finishing exercise 1

---
+++ TPO/src/main/java/org/uade/adt/SpecialQueque.java
@@ -0,0 +1,63 @@
+package org.uade.adt;
+
+import java.util.Arrays;
+
+public class SpecialQueque implements ISpecialQueque {
+
+    private Stack[] array;
+
+    private int count;
+
+    public SpecialQueque() {
+        this.array = new Stack[10000];
+        this.count = 0;
+    }
+
+
+    @Override
+    public void add(Stack a) {
+        this.array[this.count] = a;
+        this.count++;
+    }
+
+    @Override
+    public void remove() {
+        Stack newArray[] = new Stack[this.array.length - 1];
+        for (int i = 1; i < this.array.length; i++) {
+            newArray[i - 1] = this.array[i];
+        }
+        this.count --;
+        this.array = newArray;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.count == 0;
+    }
+
+    @Override
+    public Stack getTop() {
+        return this.array[0];
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Stack[] getArray() {
+        return array;
+    }
+
+    @Override
+    public String toString() {
+        return "SpecialRow{" +
+                "array=" + Arrays.toString(array) +
+                ", count=" + count +
+                '}';
+    }
+
+    public void setArray(Stack[] array) {
+        this.array = array;
+    }
+
+}
---
+++ TPO/src/main/java/org/uade/adt/Stack.java
@@ -0,0 +1,59 @@
+package org.uade.adt;
+
+import java.util.Arrays;
+
+public class Stack implements IStack {
+
+    private int[] array;
+    private int count;
+
+    public Stack() {
+        this.array = new int[10];
+        this.count = 0;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+    @Override
+    public void add(int a) {
+        this.array[this.count] = a;
+        this.count++;
+    }
+
+    @Override
+    public void remove() {
+        this.count--;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.count == 0;
+    }
+
+    @Override
+    public int getTop() {
+        return this.array[count - 1];
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "array=" + Arrays.toString(array) +
+                ", count=" + count +
+                '}';
+    }
+}
