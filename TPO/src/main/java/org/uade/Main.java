09349c93581a190f5b0c858491c30c5e2e021d0e
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2023-04-22 18:39:01 -0300

    finishing exercise 1

--- TPO/src/main/java/org/uade/Main.java
+++ TPO/src/main/java/org/uade/Main.java
@@ -1,7 +1,20 @@
 package org.uade;
 
+import org.uade.ejercicio1.Ejercicio1;
+
+import static org.uade.ejercicio1.Ejercicio1.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Ejercicio 1a");
+        System.out.println("The queue of stacks is: " + initiateQuequeMatrix());
+        System.out.println("The trace is: " + calculateTrace(initiateQuequeMatrix()));
+
+        System.out.println("Ejercicio 1b");
+        System.out.println("The transposed is: " +  generateTransposed(initiateQuequeMatrix()));
+        System.out.println();
+        System.out.println("Ejercicio 1c");
+        System.out.println("The sum of queues of stacks is: ");
+        System.out.println(sumOfQuequeArrays(initiateQuequeMatrix(),initiateQuequeMatrix()));
     }
 }
